b590c961b71f321e65111bb45596dd51cb3ed3c2
Ali Bayar <yusufertekin@gmail.com>
2013-12-17 04:00:25 +0200

    Tablo enum eklendi.

---
+++ shk/JavaSource/com/deloitte/shk/enums/Tablo.java
@@ -0,0 +1,31 @@
+package com.deloitte.shk.enums;
+
+public enum Tablo {
+    VARLIK(1L), 
+    KAYNAK(2L), 
+    KARZARAR(3L), 
+    NAKITAKIM(4L),
+    TOPLAMCALISAN(5L),
+    BORCYASLANDIRMA(6L),
+    TRAFIKSTAT(7L),
+    UCAKGIDER(8L),
+    DOVIZPOZISYON(9L),
+    UCAKFILO(10L),
+    ILISKILITARAFBAKIYE(11L),
+    ILISKILITARAFISLEM(12L),
+    ILISKILITARAFTO(13L),
+    ILISKILITARAFLAR(14L),
+    OZKAYNAK(15L),
+    KARNE(16L);
+    
+    private Long value;
+
+    private Tablo(Long value) {
+            this.value = value;
+    }
+    
+    public Long getValue(){
+    	return this.value;
+    }
+};  
+
